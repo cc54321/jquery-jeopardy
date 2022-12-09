13ad27f58125b1bc18be827f41e9274342907849
dd-code-immersives <domilamad89@gmail.com>
2022-12-09 13:04:34 -0500

    fixed event listeners and undefined element errors

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let questionItem = document.querySelectorAll('question-block');
+let questionItem = document.querySelectorAll('.question-block');
 let questionDisplay = document.querySelector('#question-display');
 let scoreDisplay = document.querySelector('#score-display');
 let userAnswer = document.querySelector('#user-answer');
 let statusMessage = document.querySelector('#status-message');
 let overlay = document.querySelector('#overlay');
 let question = document.querySelector('#question');
+let answerForm = document.querySelector("#answer-form");
+
+let CURR_ANSWER = "";
+let POINTS = "";
 
 overlay.getElementsByClassName.display = 'name';
 
@@ -33,12 +37,12 @@ let readJeopardyData = async () => {
      console.log(groupedData);
      console.log(groupedData.$200);
 
-     let answer = "";
-     let points = "";
 
      questionItem.forEach(item => {
 
           item.addEventListener('click', () => {
+
+               console.log(`item ${item} clicked`);
 
                overlay.style.display = 'block';
 
@@ -46,73 +50,70 @@ let readJeopardyData = async () => {
                userAnswer.value = "";
 
                item.classList.add('selected');
+               console.log(item.innerText);
 
-               console.log(`${groupedData[item.innerText].length}Question Worth {item.innerText}`);
+               console.log(`${groupedData[item.innerText]}Question Worth {item.innerText}`);
 
-               randomQuestionObj = groupedData[innerText][Math.floor(Math.random() = groupedData[item.innerText].legnth)];
+               randomQuestionObj = groupedData[item.innerText][Math.floor(Math.random())];
                question = randomQuestionObj['question'];
-               answer = randomQuestionObj['answer'];
+               CURR_ANSWER = randomQuestionObj['answer'];
+               console.log(question);
+               console.log(CURR_ANSWER);
 
                questionDisplay.innerHTML = question;
 
-               points = "";
+               POINTS = "";
 
           for (let i = 1; i < item.innerText.length; i++){
-               points += item.innerText[i];
-
-
-
-             //true answer verified
-               answerForm.addEventListener('submit', (event) => {
-                    event.preventDefault();
-
-                    console.log('you answered' + userAnswer.value);
-
-                    if (userAnswer.value === "" && answer !== ""){
-
-                         statusMessage.innerText = 'Enter Answer!';
-
-                    }else if(answer !== userAnswer.value.toUpperCase() === answer.toUpperCase()){
-                         questionDisplay.innerHTML = ('Correct Great!');
-
-                         overlay.style.display = 'none';
-
-                         totalScore = Number(points);
-                         scoreDisplay.innerText = totalScore;
-                         statusMessage.innerText = "";
-                         question = "";
-                         answer = "";
-                         userAnswer.value = "";
-
-                    }else if(userAnswer.value.toUpperCase() === 'yes' && !questionDisplay.innerText.incudes('wrong') && !questionDisplay.innertext.incudes('correct great!')){
-                          totalScore -= 50;
-                          scoreDisplay.innerText = totalScore;
-
-                    
-                    }else if(answer === ""){
-                         statusMessage.innerText = 'Pick A Question';
-                         userAnswer.value = "";
-
-
-                    }else{
-                         questionDisplay.innerHTML = 'Incorrect! The Correct Answer Is: ${answer}';
-                         totalScore -= Number(points);
-                         scoreDisplay.innerText = totalScore;
-                         overlay.style.display = 'none';
-
-                         question = "";
-                         answer = "";
-                         userAnswer.value = "";
-                         statusMessage.innerText = "";
-
-                    }
-               })
-
+               POINTS += item.innerText[i];
           }
+          });
+     });
+}     
+
+//true answer verified
+answerForm.addEventListener('submit', (event) => {
+     event.preventDefault();
+
+     console.log('you answered' + userAnswer.value);
+
+     if (userAnswer.value === "" && CURR_ANSWER !== ""){
+
+          statusMessage.innerText = 'Enter Answer!';
+
+     }else if(CURR_ANSWER !== userAnswer.value.toUpperCase() === CURR_ANSWER.toUpperCase()){
+          questionDisplay.innerHTML = ('Correct Great!');
+
+          overlay.style.display = 'none';
+
+          totalScore = Number(POINTS);
+          scoreDisplay.innerText = totalScore;
+          statusMessage.innerText = "";
+          question = "";
+          CURR_ANSWER = "";
+          userAnswer.value = "";
+
+     }else if(userAnswer.value.toUpperCase() === 'yes' && !questionDisplay.innerText.incudes('wrong') && !questionDisplay.innertext.incudes('correct great!')){
+               totalScore -= 50;
+               scoreDisplay.innerText = totalScore;
+
+     
+     }else if(CURR_ANSWER === ""){
+          statusMessage.innerText = 'Pick A Question';
+          userAnswer.value = "";
 
 
-          })
-     })
+     }else{
+          questionDisplay.innerHTML = 'Incorrect! The Correct Answer Is: ${answer}';
+          totalScore -= Number(POINTS);
+          scoreDisplay.innerText = totalScore;
+          overlay.style.display = 'none';
 
-}
+          question = "";
+          CURR_ANSWER = "";
+          userAnswer.value = "";
+          statusMessage.innerText = "";
+
+     }
+});
 readJeopardyData();
